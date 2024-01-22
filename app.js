8b1938e309959e1c82076518873904650a9d676a
Kobe Mertens <kobe.mertens@hotmail.com>
2024-01-22 14:10:02 +0100

    convert sending to vp to a job

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 import { app, errorHandler } from 'mu';
-import fs from 'fs';
 import bodyParser from 'body-parser';
 import { CronJob } from 'cron';
-import VP from './lib/vp';
 import { fetchCurrentUser } from "./lib/utils";
-import { getPieceUris,
+import {
+  getPieceUris,
   isAgendaItemReadyForVP,
   getMissingPieces,
-  getDecisionmakingFlowForAgendaitem
 } from './lib/agendaitem';
 import { getPieceMetadata, getSubmittedPieces } from './lib/piece';
-import { getDecisionmakingFlow } from './lib/decisionmaking-flow';
-import {
-  ENABLE_DEBUG_FILE_WRITING,
-  ENABLE_SENDING_TO_VP_API,
-  ENABLE_ALWAYS_CREATE_PARLIAMENT_FLOW,
-  PARLIAMENT_FLOW_STATUSES
-} from './config';
+import { PARLIAMENT_FLOW_STATUSES } from './config';
 
-import {
-  createOrUpdateParliamentFlow,
-  enrichPiecesWithPreviousSubmissions
-} from "./lib/parliament-flow";
-import { syncFlowsByStatus } from './lib/sync';
+import { syncFlowsByStatus } from "./lib/sync";
+import { JobManager, createJob } from "./lib/jobs";
 
 /** Schedule report generation cron job */
 const cronPattern = process.env.POLLING_CRON_PATTERN || '0 0 7 * * *';
@@ -36,7 +25,16 @@ const cronJob = new CronJob(
 	true, // start
 );
 
-const cacheClearTimeout = process.env.CACHE_CLEAR_TIMEOUT || 3000;
+const jobManager = new JobManager();
+jobManager.run();
+const runJobManagerJob = CronJob.from({
+  cronTime: "0 * * * * *",
+  onTick: function () {
+    console.log(`Jobs triggered by cron job at ${new Date().toISOString()}`);
+    jobManager.run();
+  },
+  start: true,
+});
 
 app.use(bodyParser.json());
 
@@ -107,103 +105,31 @@ app.post('/', async function (req, res, next) {
   if (!currentUser) {
     return next({ message: 'Could not find user for session', status: 404 });
   }
-  // Set default URI for debugging purposes.
-  // Default URI points to https://kaleidos-test.vlaanderen.be/dossiers/6398392DC2B90D4571CF86EA/deeldossiers
-  const decisionmakingFlowUri = await getDecisionmakingFlowForAgendaitem(agendaitemUri) ?? 'http://themis.vlaanderen.be/id/besluitvormingsaangelegenheid/6398392DC2B90D4571CF86EA';
 
-  const decisionmakingFlow = await getDecisionmakingFlow(decisionmakingFlowUri);
+  await createJob(agendaitemUri, piecesUris, comment, currentUser, isComplete);
+});
 
-  if (!decisionmakingFlow) {
-    return next({ message: 'Could not find decisionmaking flow', status: 404 });
-  }
-
-  let pieces = await getPieceMetadata(piecesUris);
-
-  if (pieces.length === 0) {
-    return next({ message: 'Could not find any files to send for decisionmaking flow', status: 404 });
-  }
-
-  if (decisionmakingFlow.parliamentFlow) {
-    pieces = await enrichPiecesWithPreviousSubmissions(decisionmakingFlow.parliamentFlow, pieces);
-  }
-
-  if (ENABLE_DEBUG_FILE_WRITING) {
-    fs.writeFileSync('/debug/pieces.json', JSON.stringify(pieces, null, 2));
-  }
-
-  let payload;
-  try {
-    payload = VP.generatePayload(decisionmakingFlow, pieces, comment);
-  } catch (error) {
-    return next({
-      message: `An error occurred while creating the payload: "${error.message}"`,
-      status: 500,
+app.get("/send-to-vp-jobs/:uuid", async function (req, res) {
+  const job = await getJob(req.params.uuid);
+  if (job) {
+    return res.status(200).send({
+      data: {
+        type: "send-to-vp-job",
+        id: job.id,
+        attributes: {
+          uri: job.uri,
+          meeting: job.meeting,
+          status: job.status,
+          created: job.created,
+        },
+      },
     });
-  }
-
-  // For debugging
-  if (ENABLE_DEBUG_FILE_WRITING) {
-    fs.writeFileSync('/debug/payload.json', JSON.stringify(payload, null, 2));
-  }
-  if (ENABLE_SENDING_TO_VP_API) {
-    let response;
-    try {
-      response = await VP.sendDossier(payload);
-    } catch (error) {
-      console.log(error.message);
-      return res.status(500).send({ message: 'Error while sending to VP: ' + error.message });
-    }
-
-    if (response.ok) {
-      const responseJson = await response.json();
-      if (ENABLE_DEBUG_FILE_WRITING) {
-        fs.writeFileSync('/debug/response.json', JSON.stringify(responseJson, null, 2));
-      }
-      await createOrUpdateParliamentFlow(responseJson, decisionmakingFlowUri, pieces, currentUser, comment, isComplete);
-
-      return setTimeout(() => {
-        res.status(200).send()
-      }, cacheClearTimeout);
-    } else {
-      if (ENABLE_DEBUG_FILE_WRITING) {
-        fs.writeFileSync('/debug/response.json', JSON.stringify(response, null, 2));
-      }
-      let errorMessage = `VP API responded with status ${response.status} and the following message: "${response.statusText}"`
-      if (response.error && response.error.message) {
-        errorMessage = response.error.message;
-      }
-      return res
-        .status(500)
-        .send({
-          message: errorMessage
-        });
-    }
   } else {
-    if (ENABLE_ALWAYS_CREATE_PARLIAMENT_FLOW) {
-      let allFiles = [];
-      for (const piece of pieces) {
-        for (const file of piece.files) {
-          allFiles.push({
-            "id": file.uri,
-            "pfls": "" + Math.floor(1000 + Math.random() * 9000), // random 4-digit pobj
-          });
-        }
-      }
-      let mockResponseJson = {
-        "MOCKED": true,
-        "status": "SUCCESS",
-        "id": decisionmakingFlowUri,
-        "pobj": "" + Math.floor(100 + Math.random() * 900), // random 3-digit pobj
-        files: allFiles
-      }
-      if (ENABLE_DEBUG_FILE_WRITING) {
-        fs.writeFileSync('/debug/response.json', JSON.stringify(mockResponseJson, null, 2));
-      }
-      await createOrUpdateParliamentFlow(mockResponseJson, decisionmakingFlowUri, pieces, currentUser, comment, isComplete);
-    }
-    return setTimeout(() => {
-      return res.status(204).end();
-    }, cacheClearTimeout);
+    return res
+      .status(404)
+      .send({
+        error: `Could not find send-to-vp-job with uuid ${req.params.uuid}`,
+      });
   }
 });
 
