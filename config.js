5dc9b29b2b4d62e1e889da55f7dd8f50578d6e7f
Sergio Fenoll <sergio@fenoll.be>
2024-03-07 12:10:23 +0100

    chore: set received flows to RECEIVED status

--- config.js
+++ config.js
@@ -69,6 +69,7 @@ const PARLIAMENT_FLOW_STATUSES = {
   COMPLETE: 'http://themis.vlaanderen.be/id/parlementaireaangelegenheid-status/018fb31c-44ad-4bf5-b01b-76de2d48abf4',
   BEING_HANDLED: 'http://themis.vlaanderen.be/id/parlementaireaangelegenheid-status/3905d9a1-c841-42fc-8a89-3b7d4ad61b4b',
   VP_ERROR: 'http://themis.vlaanderen.be/id/parlementaireaangelegenheid-status/3d03c20e-0170-43f5-840e-a541d1fd22bd',
+  RECEIVED: 'http://themis.vlaanderen.be/id/parlementaireaangelegenheid-status/879aed2f-6efa-4d7b-ab1b-13cc406b1f92',
 };
 
 /** These requirements are used to check for the existence of a documentType
